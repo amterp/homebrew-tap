3170ab55b7f62e37252251096ceb13700b72a055
Alexander Terp <alexander.terp@gmail.com>
2025-12-25 22:38:08 +1100

    keytee 0.1.3

--- Casks/keytee.rb
+++ Casks/keytee.rb
@@ -1,6 +1,6 @@
 cask "keytee" do
-  version "0.1.2"
-  sha256 "36a83f543f413fd99b1b1468ed02e6455fff85d0bf57719ded66ed10bf103319"
+  version "0.1.3"
+  sha256 "caf4c16835d66ed6f6551bb48fa0e5968175887d64a6e0b82dd9c6d33e112327"
 
   url "https://github.com/amterp/keytee/releases/download/v#{version}/KeyTee-#{version}.zip"
   name "KeyTee"
