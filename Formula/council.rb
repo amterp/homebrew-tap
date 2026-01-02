5a7bd32824d730b402f7e7768015f4c91ceb563a
Alexander Terp <alexander.terp@gmail.com>
2026-01-02 17:17:11 -0600

    council 0.1.2

--- Formula/council.rb
+++ Formula/council.rb
@@ -1,16 +1,16 @@
 class Council < Formula
   desc "CLI tool for collaborative multi-agent sessions"
   homepage "https://github.com/amterp/council"
-  version "0.1.1"
+  version "0.1.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/amterp/council/releases/download/v#{version}/council-#{version}-darwin-arm64.tar.gz"
-      sha256 "6e696d4631a790e41e923f312041daf9c5a145c9539c8883fcd26e9b0fc67f1e"
+      sha256 "2c95a2e334e6c8c926209ad682149d53bed0a7084526857b1d79197506227788"
     else
       url "https://github.com/amterp/council/releases/download/v#{version}/council-#{version}-darwin-amd64.tar.gz"
-      sha256 "47aa1b089bdcf310f4db3775cdc9537170b30400925a5d9aa9ca127ba5654b69"
+      sha256 "171974449a1db54260beed17f6583e12eee5b029eca636c7ad7c82ced2182b80"
     end
   end
 
