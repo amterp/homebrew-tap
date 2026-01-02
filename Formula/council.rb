856e463c82e740a9364c1b06cc742151630d4898
Alexander Terp <alexander.terp@gmail.com>
2026-01-02 16:05:41 -0600

    council 0.1.0

--- Formula/council.rb
+++ Formula/council.rb
@@ -7,10 +7,10 @@ class Council < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/amterp/council/releases/download/v#{version}/council-#{version}-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "facc03f0d830c0d3030fda8a908b8469763c63c6cba810f691ba969bd14b0940"
     else
       url "https://github.com/amterp/council/releases/download/v#{version}/council-#{version}-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "e3daea9efbed0294319a1a04d02949097eceea85dc04197ddf5f8934cc6a4128"
     end
   end
 
