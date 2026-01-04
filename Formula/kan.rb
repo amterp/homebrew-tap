f23e6b03099daada24434928894d2c04352c260c
Alexander Terp <alexander.terp@gmail.com>
2026-01-04 06:08:24 -0600

    kan 0.1.0

--- Formula/kan.rb
+++ Formula/kan.rb
@@ -7,10 +7,10 @@ class Kan < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/amterp/kan/releases/download/v#{version}/kan-#{version}-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "ffbded31edebe999ec791543b955e454fb5c39269e1997f77bb167ab48bf11e4"
     else
       url "https://github.com/amterp/kan/releases/download/v#{version}/kan-#{version}-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "3554b9ca36a089aba5189fe36218145dea52f4b1c8e8762218e3e52cec241fa1"
     end
   end
 
