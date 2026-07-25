af74c8ce2a29da7fcfd2a00d480feccc63b5d5e5
Alexander Terp <alexander.terp@gmail.com>
2026-07-25 11:14:54 -0500

    kan 0.28.0

--- Formula/kan.rb
+++ Formula/kan.rb
@@ -1,16 +1,16 @@
 class Kan < Formula
   desc "A kanban board that lives in your repository"
   homepage "https://github.com/amterp/kan"
-  version "0.27.1"
+  version "0.28.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/amterp/kan/releases/download/v#{version}/kan-#{version}-darwin-arm64.tar.gz"
-      sha256 "382567c4fafdda9ec9d94448afdd50225f21ebb82f181ed263de939390e0f813"
+      sha256 "b3557431ad3acf82a1367bf469d99d4a564a1dc01b1e162081d7243d737b4532"
     else
       url "https://github.com/amterp/kan/releases/download/v#{version}/kan-#{version}-darwin-amd64.tar.gz"
-      sha256 "24f9e78527f3dc19d8364e3e5ba673450d080a4c9a01653d9301cedd1cfd05ab"
+      sha256 "2b9f9f75186fdf1506231cb70f250c204487bd540c80469e760c13b41258bc22"
     end
   end
 
