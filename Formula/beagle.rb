08ad283f030d29bdf869776019d83a7bd775ee0d
Alexander Terp <alexander.terp@imc.com>
2026-08-31 08:20:28 -0500

    beagle 0.6.1

--- Formula/beagle.rb
+++ Formula/beagle.rb
@@ -1,16 +1,16 @@
 class Beagle < Formula
   desc "macOS job scheduler with profile-based multi-repo workflows"
   homepage "https://github.com/amterp/beagle"
-  version "0.6.0"
+  version "0.6.1"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/amterp/beagle/releases/download/v#{version}/beagle-#{version}-darwin-arm64.tar.gz"
-      sha256 "a17f4d55d23e4f8fa471c590cebd51df640dd7d33e4c4a276a751209c0cc2355"
+      sha256 "7163490e037e9924e4a2ec0b0ac041b47449b0feef7474e42b0df4e4c5406e57"
     else
       url "https://github.com/amterp/beagle/releases/download/v#{version}/beagle-#{version}-darwin-amd64.tar.gz"
-      sha256 "114ec42fe2db7e75b5bac1fbaca56bc27d27dcd3c9130ef142d53f259e119fc9"
+      sha256 "d8c053ba177e3eebdd5450c4b33b86fca0d5739393e8a4ff4fd657b1d553cb8b"
     end
   end
 
